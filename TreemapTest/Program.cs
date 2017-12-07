2d53c33e9ca2e4ef9887cf332fde72df1f9a7b0e
jerzy-d <jdominos@gmail.com>
2017-12-07 10:37:28 -0500

    Add project files.

---
+++ TreemapTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreemapTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] sizes = new double[] { 60, 60, 40, 30, 20, 20, 10 };
+            var tm = new Treemap(sizes, 0, 0, 60, 40);
+            tm.Squarify();
+        }
+    }
+}
